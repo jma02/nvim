6665e90da958448740ca403f24ce4c9440d27503
Jonathan Ma <ma.jonathan02@gmail.com>
2026-01-25 16:34:06 -0500

    T

--- template.cpp
+++ template.cpp
@@ -9,9 +9,9 @@ void solve() {
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int t;
-  cin >> t;
-  while (t--){
+  int T;
+  cin >> T;
+  while (T--){
     solve();
   }
 }
